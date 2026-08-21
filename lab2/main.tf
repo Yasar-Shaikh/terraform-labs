d55be83551ec7085e589d241430edb0c91d317ac
Yasar Shaikh <Yasar.shaikh17@gmail.com>
2026-08-21 08:42:12 +0530

    run

--- lab2/main.tf
+++ lab2/main.tf
@@ -11,10 +11,10 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "ntms-subnet"
+  name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.1.00/24"]
+  address_prefixes     = ["10.1.0.0/24"]
 }
 
 
