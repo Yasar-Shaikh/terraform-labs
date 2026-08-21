c10a860af593f23b39a7298d63cf900e3acfec1a
Yasar Shaikh <Yasar.shaikh17@gmail.com>
2026-08-21 08:22:14 +0530

    2nd changes

--- lab2/main.tf
+++ lab2/main.tf
@@ -4,8 +4,17 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  resource_group_name = "ntms-rg1"
-  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.1.0.0/16"]
   location            = azurerm_resource_group.rg.location
-  name                = "ntms-vnet1"
+  name                = "ntms-vnet"
 }
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "ntms-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.00/24"]
+}
+
+
