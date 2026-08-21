17a67192f0be91fbe5024972ac22777118827e71
Yasar Shaikh <Yasar.shaikh17@gmail.com>
2026-08-21 09:48:18 +0530

    changes

--- lab2/main.tf
+++ lab2/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "ntms-rg1"
-  location = "East US"
+  name     = "${var.business_unit}-${var.environment}-${var.rg_name}"
+  location = var.rg_location  
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = "${var.business_unit}-${var.environment}-${var.vnet_name}"
   address_space       = ["10.1.0.0/16"]
   location            = azurerm_resource_group.rg.location
   name                = "ntms-vnet"
---
+++ lab2/variable.tf
@@ -0,0 +1,37 @@
+# resource group name
+variable "rg_name" {
+  description = "Name of the resource group"
+  type        = string
+  default = "myrg"
+}
+
+#virtual network name
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+  default = "myvnet"
+}
+
+
+# resource group location
+variable "rg_location" {
+  description = "Location of the resource group"
+  type        = string
+  default = "East US"
+}
+
+#Business unit Name
+variable "business_unit" {
+  description = "Name of the business unit"
+  type        = string
+  default = "IT"
+
+}   
+
+#Environment Name
+variable "environment" {
+  description = "Name of the environment"
+  type        = string
+  default = "Dev"
+}
+ 
